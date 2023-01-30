42fe0d459c492f2d5b447b349536f48bd2b638ef
YukiTome <tome.okinawa@gmail.com>
2023-01-30 23:06:20 +0900

    マスを二重ループで表示

--- src/App.js
+++ src/App.js
@@ -31,24 +31,26 @@ function Board({ xIsNext, squares, onPlay }) {
   } else {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
+  let cnt = 0;
 
   return (
     <>
-      <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      <div className="status">{status}
+      {
+        Array(3).fill(0).map((val, i) => {
+          return (
+            <div className="board-row">
+              {
+               Array(3).fill(0).map((val2, j) => {
+                return(
+                  <Square value={squares[ (3*i)+j ]} onSquareClick={() => handleClick( (3*i)+j )} />
+                  );
+                })
+              }
+            </div>
+          );
+        })
+      }
       </div>
     </>
   );
